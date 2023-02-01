653957c3d9cce353fb4840efe404e85dd63c9ac1
Daniel Unverricht <daniel.unverricht@pentlandfirth.com>
2023-02-01 15:06:16 +0100

    remove log-dependency

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/dunv/uenv
 
 go 1.17
-
-require github.com/dunv/ulog v1.0.27
-
-require github.com/dunv/uhelpers v1.0.14 // indirect
--- uenv.go
+++ uenv.go
@@ -4,18 +4,11 @@ import (
 	"bufio"
 	"os"
 	"strings"
-
-	"github.com/dunv/ulog"
 )
-
-func init() {
-	ulog.AddReplaceFunction("github.com/dunv/uenv.SetDotEnv", "uenv")
-}
 
 func SetDotEnv() {
 	f, err := os.Open(".env")
 	if err != nil {
-		ulog.Infof("not parsing .env (%s)", err)
 		return
 	}
 
@@ -34,14 +27,10 @@ func SetDotEnv() {
 
 		parts := strings.SplitN(line, "=", 2)
 		if len(parts) != 2 {
-			ulog.Infof("skipping %s (no two parts)", parts[0])
 			continue
 		}
 		if err := os.Setenv(parts[0], parts[1]); err != nil {
-			ulog.Infof("could not set %s (%s)", parts[0], err)
 			continue
 		}
-
-		ulog.Infof("setenv %s=%s", parts[0], parts[1])
 	}
 }
